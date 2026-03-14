2388dbc814af4533fd0e8977abf939aa2c33baa5
김성일 <kimsungil322@gmail.com>
2026-03-14 09:51:37 +0900

    불필요 화면/라우트 정리 및 README 개선

--- lib/shared/api/endpoints.dart
+++ lib/shared/api/endpoints.dart
@@ -22,16 +22,10 @@ class ApiEndpoints {
   static String photoVisibility(String photoId) =>
       '/photos/$photoId/visibility';
   static String photoPrimary(String photoId) => '/photos/$photoId/primary';
-  static const String subscription = '/subscription';
-  static const String subscriptionCancel = '/subscription/cancel';
-  static const String subscriptionPause = '/subscription/pause';
-  static const String subscriptionResume = '/subscription/resume';
-  static const String subscriptionIsActive = '/subscription/is-active';
   static String emailVerificationRequest(String environmentId) =>
       '/environments/$environmentId/email-verifications/request';
   static String emailVerificationConfirm(String environmentId) =>
       '/environments/$environmentId/email-verifications/confirm';
-  static const String health = '/health';
   static String userBlock(String userId) => '/users/block/$userId';
 
   // 커뮤니티 (학교별, 누구나 진입 가능)
