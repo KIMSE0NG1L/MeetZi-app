ac89a8ab1e1490d0fa968f623b6177a2226f9dea
김성일 <kimsungil322@gmail.com>
2026-03-22 17:01:40 +0900

    채팅 동기화와 매칭 수락 알림 흐름 개선

--- lib/shared/utils/app_config.dart
+++ lib/shared/utils/app_config.dart
@@ -1,4 +1,4 @@
 /// 로컬: http://PC_IP:3000  |  Cloudflare: https://xxxx.trycloudflare.com  |  ngrok: https://xxxx.ngrok-free.dev
 class AppConfig {
-  static const String baseUrl = 'https://hurtlingly-blatant-tari.ngrok-free.dev';
+  static const String baseUrl = 'https://nearo-server-zpnz46qxuq-du.a.run.app';
 }
