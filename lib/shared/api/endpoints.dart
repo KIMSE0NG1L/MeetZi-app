d0e9081c4533df7f9affb273fd31fa7154df31d9
김성일 <kimsungil322@gmail.com>
2026-03-29 20:51:00 +0900

    커뮤니티를 통합 피드로 전환

--- lib/shared/api/endpoints.dart
+++ lib/shared/api/endpoints.dart
@@ -4,7 +4,7 @@ class ApiEndpoints {
   static const String authProfile = '/auth/profile';
   static const String authRefresh = '/auth/refresh';
   static const String avatarInit = '/users/me/avatar/init';
-    static const String accountDelete = '/users/me';
+  static const String accountDelete = '/users/me';
   static const String matchingRequest = '/matching/request';
   static const String matchingCancel = '/matching/cancel';
   static const String matchingActive = '/matching/active';
@@ -20,7 +20,8 @@ class ApiEndpoints {
   static const String sharedChatPosts = '/shared-chat-posts';
   static const String sharedChatMine = '/shared-chat-posts/me';
   static String sharedChatPost(String id) => '/shared-chat-posts/$id';
-  static String sharedChatConsent(String id) => '/shared-chat-posts/$id/consent';
+  static String sharedChatConsent(String id) =>
+      '/shared-chat-posts/$id/consent';
   static const String photos = '/photos';
   static const String photosUpload = '/photos/upload';
   static String photoById(String photoId) => '/photos/$photoId';
@@ -33,9 +34,9 @@ class ApiEndpoints {
       '/environments/$environmentId/email-verifications/confirm';
   static String userBlock(String userId) => '/users/block/$userId';
 
-  // 而ㅻ??덊떚 (?숆탳蹂? ?꾧뎄??吏꾩엯 媛??
   static String community(String environmentId) => '/communities/$environmentId';
-  static String communityPosts(String environmentId) => '/communities/$environmentId/posts';
+  static String communityPosts(String environmentId) =>
+      '/communities/$environmentId/posts';
   static String communityPost(String environmentId, String postId) =>
       '/communities/$environmentId/posts/$postId';
   static String communityPostDelete(String environmentId, String postId) =>
@@ -44,17 +45,44 @@ class ApiEndpoints {
       '/communities/$environmentId/posts/$postId/comments';
   static String communityPostLike(String environmentId, String postId) =>
       '/communities/$environmentId/posts/$postId/like';
-  static String communityPostCommentLike(String environmentId, String postId, String commentId) =>
+  static String communityPostCommentLike(
+    String environmentId,
+    String postId,
+    String commentId,
+  ) =>
       '/communities/$environmentId/posts/$postId/comments/$commentId/like';
-  static String communityPostCommentDelete(String environmentId, String postId, String commentId) =>
+  static String communityPostCommentDelete(
+    String environmentId,
+    String postId,
+    String commentId,
+  ) =>
       '/communities/$environmentId/posts/$postId/comments/$commentId';
   static String communityPostVote(String environmentId, String postId) =>
       '/communities/$environmentId/posts/$postId/vote';
 
-  // ?좉퀬
+  static const String communityGlobalPosts = '/communities/global/posts';
+  static String communityGlobalPost(String postId) =>
+      '/communities/global/posts/$postId';
+  static String communityGlobalPostDelete(String postId) =>
+      '/communities/global/posts/$postId';
+  static String communityGlobalPostComments(String postId) =>
+      '/communities/global/posts/$postId/comments';
+  static String communityGlobalPostLike(String postId) =>
+      '/communities/global/posts/$postId/like';
+  static String communityGlobalPostCommentLike(
+    String postId,
+    String commentId,
+  ) =>
+      '/communities/global/posts/$postId/comments/$commentId/like';
+  static String communityGlobalPostCommentDelete(
+    String postId,
+    String commentId,
+  ) =>
+      '/communities/global/posts/$postId/comments/$commentId';
+  static String communityGlobalPostVote(String postId) =>
+      '/communities/global/posts/$postId/vote';
+
   static const String reportCommunityPost = '/report/community-post';
   static const String reportCommunityComment = '/report/community-comment';
   static const String notices = '/notices';
 }
-
-
