0e7e85dd3f75029d82e89dcb0ee0e1f6840da889
dongjun-03 <whehdwns1541@naver.com>
2026-04-01 23:00:31 +0900

    초기 게시판 전체학교로 변경

--- lib/features/matching_board/screens/matching_board_screen.dart
+++ lib/features/matching_board/screens/matching_board_screen.dart
@@ -162,10 +162,10 @@ class _MatchingBoardScreenBodyState extends State<_MatchingBoardScreenBody> {
     }
   }
 
-  /// 저장된 전체/우리대학 선택을 읽어서 해당 목록을 불러옴. 기본은 우리 학교.
+  /// 저장된 전체/우리대학 선택을 읽어서 해당 목록을 불러옴. 기본은 전체 대학.
   Future<void> _loadScopeAndFetch() async {
     final prefs = await SharedPreferences.getInstance();
-    final scopeAll = prefs.getBool(_keyScopeAllUniversities) ?? false;
+    final scopeAll = prefs.getBool(_keyScopeAllUniversities) ?? true;
     if (!mounted) return;
     setState(() => _isShowingAllUniversities = scopeAll);
     if (scopeAll) {
